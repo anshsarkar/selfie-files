4a59988651aebf18e6aba14b2aafbff42dbd9203
Ansh Sarkar <ansh.sarkar@navi.com>
2023-01-30 18:16:55 +0530

    Tensorflow bug fix

--- helper/align_images.py
+++ helper/align_images.py
@@ -6,7 +6,8 @@ from scipy import misc
 import sys
 import os
 import argparse
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import numpy as np
 sys.path.append(os.path.join(os.path.dirname(__file__),'..', ''))
 import facenet.src.facenet as facenet
@@ -30,7 +31,7 @@ def main(args):
     print('Creating networks and loading parameters')
     
     with tf.Graph().as_default():
-        gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=args.gpu_memory_fraction)
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_memory_fraction)
         sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
         with sess.as_default():
             pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
